42e2a975b03e4cfb4ef8c637481e815572242ced
17920368 <75653961+17920368@users.noreply.github.com>
2021-01-27 13:59:33 -0600

    Add files via upload

--- static/js/productos_listar.js
+++ static/js/productos_listar.js
@@ -1,37 +1,17 @@
-var contador=0;
-var conArray=0;
-var cont=0;
-var idElement=0;
-
 $(document).ready(function (){
     $(".hidden_images").each(function (){
         let id= $(this).attr("id");
         let imagenes = $("#"+ id).val().split(",");
-        let imagen="<img class='card-image' src='/uploads/" + imagenes[0] +
-             "' alt='imagen' width='200' height='200' id='imagen_"+id+"'>";
-        $("#divImages_" + id).append(imagen);
+        for (let index = 0; index < imagenes.length; index++) {
+            let imagen="<a href='/uploads/" + imagenes[index] +
+            "' data-lightbox='/uploads/" + imagenes[index] +
+            "'><img class='card-image columna' src='/uploads/" + imagenes[index] +
+            "' alt='imagen' width='170' height='170' id='imagen'> </a>";
+       $("#divImages_" + id).append(imagen);  
+            
+        }
+
     });
     
 }
 )
-
-function allImages(imgAll, idProduct){
-    let idAux;
-    let imagenes = imgAll.split(",");
-    conArray=imagenes;
-    cont=imagenes.length;
-    contador--;
-    if(contador==-1){
-        contador=cont-1;
-        var img=document.getElementById("imagen_"+idProduct);
-        img.src="/uploads/"+conArray[contador];
-    }else{
-    var img=document.getElementById("imagen_"+idProduct);
-    img.src="/uploads/"+conArray[0];
-    contador=0;
-    }
-    
-
-    
-}
-
